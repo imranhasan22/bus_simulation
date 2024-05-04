4fa6997516a93141a0615467a0e6ee06025e9781
Imran Hasan <imran258hasan@gmail.com>
2024-05-04 20:49:08 +0600

    Stopage.java

--- Stopage.java
+++ Stopage.java
@@ -3,21 +3,21 @@ public class Stopage{
     int pos_X,pos_Y;
     char pos_symbol;
     
-     Stopage[] getStopage(BusSimulation busSimulation_object){
+     Stopage[] getStopage(BusSimulation bus_obj){
         Scanner scanner = new Scanner(System.in);
         System.out.print("How many stopages: ");
-        busSimulation_object.stopage_size = scanner.nextInt();
+        bus_obj.stopage_size = scanner.nextInt();
         
-        Stopage[] stopageArray = new Stopage[busSimulation_object.stopage_size];
-        int[] X = new int[busSimulation_object.stopage_size];
-        int[] Y = new int[busSimulation_object.stopage_size];
+        Stopage[] stopageArray = new Stopage[bus_obj.stopage_size];
+        int[] X = new int[bus_obj.stopage_size];
+        int[] Y = new int[bus_obj.stopage_size];
         char word='A';
-        for (int i=0;i<busSimulation_object.stopage_size;i++) 
+        for (int i=0;i<bus_obj.stopage_size;i++) 
         {
             System.out.print((i+1)+"th stopage coordinate X & Y: ");
             X[i] = scanner.nextInt();
             Y[i] = scanner.nextInt();
-            busSimulation_object.board_box[X[i]][Y[i]]=word;
+            bus_obj.board_box[X[i]][Y[i]]=word;
 
             Stopage stopage = new Stopage();
             stopageArray[i]=stopage;
