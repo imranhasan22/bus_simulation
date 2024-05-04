e611c58ab66ea7d7076e4fa0a63e44a76d5d961e
Imran Hasan <imran258hasan@gmail.com>
2024-05-04 20:47:18 +0600

    Board.java

--- Board.java
+++ Board.java
@@ -1,32 +1,21 @@
 public class Board {
-    public void initializeBoard(BusSimulation busSimulation) {
-        for (int row = 0; row < busSimulation.ROWS; row++) {
-            for (int col = 0; col < busSimulation.COLUMNS; col++) {
-                if (row == 0 || row == busSimulation.ROWS - 1 || col == 0 || col == busSimulation.COLUMNS - 1) {
-                    busSimulation.board_box[row][col] = '#';
+    public void initialize_board(BusSimulation busSimulation) {
+        for (int i = 0; i < busSimulation.row; i++) {
+            for (int j = 0; j < busSimulation.col; j++) {
+                if (i == 0 || i == busSimulation.row - 1 || j == 0 || j== busSimulation.col - 1) {
+                    busSimulation.board_box[i][j] = '#';
                 } else {
-                    busSimulation.board_box[row][col] = ' ';
+                    busSimulation.board_box[i][j] = ' ';
                 }
             }
         }
     }
-
-    public void printBoard(BusSimulation busSimulation) {
-        for (int row = 0; row < busSimulation.ROWS; row++) {
-            for (int col = 0; col < busSimulation.COLUMNS; col++) {
-                System.out.print(busSimulation.board_box[row][col]);
+    public void print_board(BusSimulation busSimulation) {
+        for (int i = 0; i< busSimulation.row; i++) {
+            for (int j = 0; j < busSimulation.col; j++) {
+                System.out.print(busSimulation.board_box[i][j]);
             }
             System.out.println();
         }
     }
-
-    // public void clearBoardMarks(BusSimulation busSimulation) {
-    //     for (int row = 0; row < busSimulation.ROWS; row++) {
-    //         for (int col = 0; col < busSimulation.COLUMNS; col++) {
-    //             if (busSimulation.board_box[row][col] == '*') {
-    //                 busSimulation.board_box[row][col] = ' ';
-    //             }
-    //         }
-    //     }
-    // }
 }
