a95f134067786b317a9991c213ac00ae6d6ae41b
Imran Hasan <imran258hasan@gmail.com>
2024-05-04 20:48:28 +0600

    Distance.java

--- Distance.java
+++ Distance.java
@@ -3,20 +3,20 @@ public class Distance{
      char Start,End;
      float Weight;
 
-    public Distance[] getDistance(BusSimulation busSimulation_object){
+    public Distance[] get_dist(BusSimulation bus_obj){
         Scanner scanner = new Scanner(System.in);
         System.out.print("How many distance: ");
-        busSimulation_object.distance_size=scanner.nextInt();
+        bus_obj.distance_size=scanner.nextInt();
         
-        Distance[] distanceArray = new Distance[busSimulation_object.distance_size];
-        for(int i=0;i<busSimulation_object.distance_size;i++){
+        Distance[] dist_arr = new Distance[bus_obj.distance_size];
+        for(int i=0;i<bus_obj.distance_size;i++){
             System.out.print("Enter Starting Stopage, Ending Stopage, Distance: ");
             Distance dist = new Distance();
-            distanceArray[i]=dist;
-            distanceArray[i].Start=scanner.next().charAt(0);
-            distanceArray[i].End=scanner.next().charAt(0);
-            distanceArray[i].Weight=scanner.nextFloat();
+            dist_arr[i]=dist;
+            dist_arr[i].Start=scanner.next().charAt(0);
+            dist_arr[i].End=scanner.next().charAt(0);
+            dist_arr[i].Weight=scanner.nextFloat();
         }
-        return distanceArray;
+        return dist_arr;
     }
 }
