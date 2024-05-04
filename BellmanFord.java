f8124f65af0b833bb901f3fa6bbdc857198ef0c4
Imran Hasan <imran258hasan@gmail.com>
2024-05-04 20:46:35 +0600

    BellmanFord.java

--- BellmanFord.java
+++ BellmanFord.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 class PathDistance {
@@ -17,14 +18,14 @@ class PathDistance {
 public class BellmanFord {
     int vertices, edge;
     int edge_ind = 0;
-    PathDistance[] distanceArray;
+    PathDistance[] dist_arr;
     int[] distances;
     int[] previous;
 
     BellmanFord(int vertices, int edge) {
         this.vertices = vertices;
         this.edge = edge;
-        distanceArray = new PathDistance[edge];
+        dist_arr = new PathDistance[edge];
         distances = new int[vertices];
         previous = new int[vertices];
         Arrays.fill(distances, Integer.MAX_VALUE);
@@ -32,79 +33,64 @@ public class BellmanFord {
     }
 
     void addEdge(char start, char end, float weight) {
-        distanceArray[edge_ind++] = new PathDistance(start, end, weight);
+        dist_arr[edge_ind++] = new PathDistance(start, end, weight);
     }
 
-    void shortestPath(char source, char destination) {
+    List<Character> shortestPath(char source, char destination) {
         distances[source - 'A'] = 0;
         for (int i = 0; i < vertices - 1; i++) {
             for (int j = 0; j < edge; j++) {
-                char u = distanceArray[j].start;
-                char v = distanceArray[j].end;
-                float weight = distanceArray[j].weight;
+                char u = dist_arr[j].start;
+                char v = dist_arr[j].end;
+                float weight = dist_arr[j].weight;
                 if (distances[u - 'A'] != Integer.MAX_VALUE && distances[u - 'A'] + weight < distances[v - 'A']) {
-                    distances[v - 'A'] = distances[u - 'A'] + (int)weight;
+                    distances[v - 'A'] = distances[u - 'A'] + (int) weight;
                     previous[v - 'A'] = u - 'A';
                 }
             }
         }
 
-        
-        for(int j=0;j<edge;j++)
-        {
-            char u=distanceArray[j].start;
-            char v=distanceArray[j].end;
-            float weight=distanceArray[j].weight;
-            if(distances[u-'A']!=Integer.MAX_VALUE&& distances[u-'A']+weight<distances[v-'A'])
-            {
+        for (int j = 0; j < edge; j++) {
+            char u = dist_arr[j].start;
+            char v = dist_arr[j].end;
+            float weight = dist_arr[j].weight;
+            if (distances[u - 'A'] != Integer.MAX_VALUE && distances[u - 'A'] + weight < distances[v - 'A']) {
                 System.out.println("negative weight cycle");
+                return new ArrayList<>();
+            }
+        }
+
+        List<Character> path = new ArrayList<>();
+        int current = destination - 'A';
+        while (current != source - 'A') {
+            path.add((char) (current + 'A'));
+            current = previous[current];
+            if (current == -1) {
+                System.out.println("No path found");
+                return new ArrayList<>();
+            }
+        }
+        path.add(source);
+        Collections.reverse(path);
+        return path;
+    }
+
+    void printPath(int source, int current, int destination) {
+        List<Character> path = new ArrayList<>();
+        while (current != source) {
+            path.add((char) (current + 'A'));
+            current = previous[current];
+            if (current == -1) {
+                System.out.println("No path found");
                 return;
             }
         }
-        System.out.println("shortest path from"+source+" to other nodfe: ");
-        for(int i=0;i<vertices;i++)
-        {
-            if(distances[i]!=Integer.MAX_VALUE)
-            {
-                System.out.println("Distance to"+(char) (i+'A')+" :"+distances[i]);
-
-            }
-            else
-            {
-                System.out.println("Distance to "+(char) (i+'A')+" :infinity");
-            }
-            }
-            
-            if(distances[destination-'A']!=Integer.MAX_VALUE)
-            {
-                System.out.print("shortest path from "+source+" to"+destination+" : ");
-                printPath(source-'A',destination-'A',destination-'A');
-                System.out.println();
-
-            }
-            else
-            {
-                System.out.println("No path found"+source+" to "+destination+ " ..");
-            }
-
-            }
-
-        void printPath(int source, int current, int destination) {
-            List<Character> path = new ArrayList<>();
-            while (current != source) {
-                path.add((char) (current + 'A'));
-                current = previous[current];
-                if (current == -1) {
-                    System.out.println("No path found");
-                    return;
-                }
-            }
-            path.add((char) (source + 'A'));
-            for (int i = path.size() - 1; i >= 0; i--) {
-                System.out.print(path.get(i));
-                if (i != 0) {
-                    System.out.print(" -> ");
-                }
+        path.add((char) (source + 'A'));
+        for (int i = path.size() - 1; i >= 0; i--) {
+            System.out.print(path.get(i));
+            if (i != 0) {
+                System.out.print(" -> ");
             }
         }
     }
+}
